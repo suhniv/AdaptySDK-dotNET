5cb595baeeabd12eb3ef3b302635102fbed5b1f6
suhniv <91676682+suhniv@users.noreply.github.com>
2022-03-28 10:23:33 +0300

    Delete XamarinAdaptySDK/XamarinAdaptySDK/Plugin.Adapty directory

--- XamarinAdaptySDK/XamarinAdaptySDK/Plugin.Adapty/Models/Paywall.shared.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Plugin.Adapty.Models
-{
-    public class Paywall
-    {
-        public string? DeveloperId { get; set; }
-        public Product[]? Products { get; set; }
-        public string? CustomPayload { get; set; }
-    }
-}
